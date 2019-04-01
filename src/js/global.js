cf6c107c06d54b98ee2e96c2c1fec08df6d67e31
vasiliy <vasili_setyn_mail_ru@mail.ru>
2019-04-01 20:17:16 +0300

    one more stap

--- src/js/global.js
+++ src/js/global.js
@@ -7,6 +7,11 @@ const createTitle = require_file_fromJS("callbacks/title")
 const toArrays = require_file_fromJS("callbacks/toArr")
 const deepOutput = require_file_fromJS("callbacks/deepOutput")
 const stringfullDataOutput = require_file_fromJS("callbacks/comaStr")
-const keysDetected = require_file_fromJS("callbacks/event")
+const buildInitial_KeyData = require_file_fromJS("callbacks/initialKeys")
+const binarySearch = require_file_fromJS("callbacks/binarySearch")
+const printPressedKeysData = require_file_fromJS("callbacks/printKeys")
 
 const content = document.querySelector('div.content')
+
+// Initial keys's data structure [{},{},{}...]
+let keys = buildInitial_KeyData()
